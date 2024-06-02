d36c4fe470547cbc75cb3919d1ab2c537b0a3947
beck <bibek.dhakal@imark.com.np>
2024-06-02 16:15:51 +1000

    user create route issue: should be grouped into tenant middleware

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('universities', [ApiController::class, 'universities']);
-Route::post('user/create', [UserController::class, 'store']);
 Route::group(['middleware' => ['switch.tenant']], function () {
+    Route::post('user/create', [UserController::class, 'store']);
     Route::post('login', [UserController::class, 'authenticate']);
     Route::get('campuses', [ApiController::class, 'get_campuses']);
     Route::post('reset-password', [UserController::class, 'reset_password']);
